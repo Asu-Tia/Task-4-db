7be9c89245d82b3d02cf184cf5988b1d1509a9b2
Gergő Csongor Vincze <65530422+Asu-Tia@users.noreply.github.com>
2021-03-24 16:30:08 +0100

    Partial solution.

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,46 @@
 package com.codecool.database;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class RadioCharts {
-    //TODO: implement this class
+
+    private final String url;
+    private final String user;
+    private final String password;
+
+    public RadioCharts(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getMostPlayedSong() {
+        String query = "SELECT song FROM music_broadcast GROUP BY song ORDER BY SUM(times_aired) DESC LIMIT 1";
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             ResultSet rs = connection.createStatement().executeQuery(query)) {
+            if (rs.next()) {
+                return rs.getString(1);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return "";
+    }
+
+    public String getMostActiveArtist() {
+        String query = "SELECT artist FROM music_broadcast GROUP BY song ORDER BY COUNT(*) DESC LIMIT 1";
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             ResultSet rs = connection.createStatement().executeQuery(query)) {
+            if (rs.next()) {
+                return rs.getString(1);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return "";
+    }
 }
